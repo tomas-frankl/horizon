2f817985b521d061213d257c0f39fe59540b1286
Tomas Frankl <tomas.frankl@gmail.com>
2013-09-26 20:16:23 +0200

    Unnecessary files removed

--- application/src/com/franktom/horizon/IFolderItemListener.java
+++
@@ -1,9 +0,0 @@
-package com.franktom.horizon;
-
-import java.io.File;
-
-public interface IFolderItemListener {
-
-    void OnCannotFileRead(File file);//implement what to do folder is Unreadable
-    void OnFileClicked(File file);//What to do When a file is clicked
-}
